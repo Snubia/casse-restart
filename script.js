6327e8e9f7b878c25ea0446d490e225fb78f4c8e
Sandrine <nunubabila@gmail.com>
2021-01-03 18:09:13 -0800

    draw canvas

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
-const rules = document.getElementById('rules')
+const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d')
+
+// Create balls properties
+
+const ball = {
+    x: canvas.width /2,
+    y: canvas.height /2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4
+}
+
+// Draw the ball in the canvas
+
+function drawball() {
+    
+}
 
 // rules and close evt handlers
 
